275afb4f5c72af5b9f293c130824756859c0109e
JSH1990 <tmdgus21@naver.com>
2026-04-20 16:14:34 +0900

    first

---
+++ src/main/java/com/movetogether/MoveTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.movetogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoveTogetherApplication.class, args);
+    }
+
+}
