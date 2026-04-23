5783eafe5e2d23a42a4d926c1c3bc2746e064a32
JSH1990 <tmdgus21@naver.com>
2026-04-23 09:03:34 +0900

    Handling the signUp form submit

--- src/main/java/com/movetogether/modules/acount/AccountController.java
+++ src/main/java/com/movetogether/modules/acount/AccountController.java
@@ -1,14 +1,44 @@
 package com.movetogether.modules.acount;
 
+import com.movetogether.modules.acount.form.SignUpForm;
+import com.movetogether.modules.acount.validator.SignUpFormValidator;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountService accountService;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
+        model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        Account account = accountService.processNewAccount(signUpForm);
+        accountService.login(account);
+        return "redirect:/";
+    }
+
 }
