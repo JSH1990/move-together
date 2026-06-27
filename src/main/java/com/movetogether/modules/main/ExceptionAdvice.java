e477946d70014bc235a128dcb80b0e21c99b4ca2
JSH1990 <tmdgus21@naver.com>
2026-06-27 10:54:57 +0200

    fix: return 403 for unauthorized club settings access

--- src/main/java/com/movetogether/modules/main/ExceptionAdvice.java
+++ src/main/java/com/movetogether/modules/main/ExceptionAdvice.java
@@ -4,14 +4,17 @@ import com.movetogether.modules.account.Account;
 import com.movetogether.modules.account.CurrentAccount;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
 @ControllerAdvice
 public class ExceptionAdvice {
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     public String handleRuntimeException(@CurrentAccount Account account, HttpServletRequest req, RuntimeException e) {
         if (account != null) {
             log.info("'{}' requested '{}'", account.getNickname(), req.getRequestURI());
